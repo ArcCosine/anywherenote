38af1fa58f4dead5626ac5a209fa7a53a551a6f1
ArcCosine <40669+ArcCosine@users.noreply.github.com>
2022-04-06 00:59:22 +0900

    [Update] Update webpack-package's.

---
+++ dist/anywherenote.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/base.js":
+/*!*********************!*\
+  !*** ./src/base.js ***!
+  \*********************/
+/***/ (() => {
+
+eval("const init = () => {\n    console.log(\"first commit\");\n}\n\ninit();\n\n\n//# sourceURL=webpack://anywhernote/./src/base.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/base.js"]();
+/******/ 	
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
 const path = require("path");
 
 module.exports = {
+    mode: "development",
     entry: "./src/base.js",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "anywherenote.js"
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname,"dist"),
-        publicPath: "",
-        watchContentBase: true
-    },
-    devtool: "source-map"
-}
+    }
+};
